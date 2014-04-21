69a0d7719561d646a85306c5f9799be21c123b0a
Maureen <maureen.k.dugan@gmail.com>
2014-04-21 11:54:57 -0700

    update spec_helper with VCR filters

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,17 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
+<<<<<<< HEAD
+=======
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+  c.filter_sensitive_data('<twilio account sid>') { ENV['TWILIO_ACCOUNT_SID'] }
+  c.filter_sensitive_data('<twilio auth token>') { ENV['TWILIO_AUTH_TOKEN'] }
+end
+
+>>>>>>> c3ba788... update spec helper with VCR filters for senitive data
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 
