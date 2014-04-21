c47561b9e890494dca91f27861c26c273ee0b522
Maureen <maureen.k.dugan@gmail.com>
2014-04-21 14:05:18 -0700

    update factories with proper Faker methods

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,6 +1,13 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  sequence :body do |b|
+    body = Faker::Lorem.characters(20)
+  end
+
   factory :message do
+    body
+    to '6072620716'
+    from '5038289722'
   end
 end
